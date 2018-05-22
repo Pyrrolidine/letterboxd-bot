67711201c9d66f813e49823d2ea50faad632153f
Thomas Philippe <thomas27950@gmail.com>
2018-05-22 22:43:45 +0200

    fix film search with apostrophe

--- src/lbxd/film.py
+++ src/lbxd/film.py
@@ -42,6 +42,7 @@ class Film(object):
                   + api_key
         if self.has_year:
             keywords = ' '.join(keywords.split()[:-1])
+        keywords = keywords.replace("’", "").replace("'", "")
         query = urllib.parse.quote_plus(keywords)
         api_url += "&query=" + query
         api_url += "&year=" + self.input_year if self.has_year else ''
