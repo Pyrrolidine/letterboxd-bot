54dd09d8922c52b2059fcda2a76394cff81c704b
Thomas Philippe <thomas27950@gmail.com>
2018-08-18 18:20:10 +0200

    add recent activity description

--- src/lbxd/diary.py
+++ src/lbxd/diary.py
@@ -1,15 +1,55 @@
 from .core import api
-from .exceptions import LbxdNotFound
 import discord
 
 
 class Diary(object):
     def __init__(self, user):
         self.user = user
+        self.description = self.get_activity()
+
+    def get_activity(self):
+        params = {
+            'include': 'DiaryEntryActivity',
+            'where': 'OwnActivity',
+            'perPage': 100
+        }
+        response = api.api_call('member/{}/activity'.format(self.user.lbxd_id),
+                                params)
+        description = ''
+        n_entries = 0
+        for entry in response.json()['items']:
+            if n_entries > 4:
+                break
+            if not entry.get('diaryEntry'):
+                continue
+            diary_entry = entry['diaryEntry']
+            n_entries += 1
+            for link in diary_entry['links']:
+                if link['type'] == 'letterboxd':
+                    entry_url = link['url']
+                    break
+            description += '**[' + diary_entry['film']['name']
+            film_year = ' ({})'.format(diary_entry['film'].get('releaseYear'))
+            if film_year:
+                description += film_year
+            description += ']({})**\n'.format(entry_url)
+            if diary_entry.get('diaryDetails'):
+                description += diary_entry['diaryDetails']['diaryDate'] + ' '
+            if diary_entry.get('rating'):
+                description += '★' * int(diary_entry['rating'])
+                if abs(diary_entry['rating'] * 10) % 10:
+                    description += '½'
+            if diary_entry['like']:
+                description += ' ♥'
+            description += '\n'
+        return description
 
     def create_embed(self):
         title = 'Recent diary activity from {}'.format(self.user.display_name)
         diary_embed = discord.Embed(
-            title=title, url=self.user.url, colour=0xd8b437, description='')
+            title=title,
+            url=self.user.url,
+            colour=0xd8b437,
+            description=self.description)
         diary_embed.set_thumbnail(url=self.user.avatar_url)
         return diary_embed
