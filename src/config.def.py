6dc52f3fe9c619889f0f8aa465eef483a120c994
Thomas Philippe <thomas27950@gmail.com>
2018-11-26 23:25:22 +0100

    move conf values to python file

---
+++ src/config.def.py
@@ -0,0 +1,50 @@
+SETTINGS = dict()
+SETTINGS["cloudinary"] = {
+    "api_key": "",
+    "api_secret": "",
+    "cloud_name": ""
+}
+SETTINGS["discord"] = ""
+SETTINGS["fixed_crew_search"] = {
+    "Johnnie To": "2xkp",
+    "McG": "2Rst"
+}
+SETTINGS["fixed_film_search"] = {
+    "Boudu": "IDg",
+    "Intolerance": "25Qk",
+    "King of Kong": "1P2y",
+    "Love": "9brE",
+    "Red Dragon": "1Yrs",
+    "Salo": "237s",
+    "Snow White": "2arO",
+    "Super": "QgO",
+    "Tabu": "29aM",
+    "The Wrong Man": "1AqK"
+}
+SETTINGS["fixed_user_search"] = {
+    "Supe": "bdJr"
+}
+SETTINGS["help"] = {
+    "!film/!movie/!f <film-name> (<year>)":
+    "Searches a film on Letterboxd. The year is optional.",
+    "!crew/!c/!director/!d/!actor/!a <name>":
+    "Searches a crew person.",
+    "!user/!u <username>":
+    "Displays the details of a Letterboxd member.",
+    "!review/!r <username> <film-name> (<year>)":
+    "Displays the logged entries (review, diary) from a member.",
+    "!list/!l <username> <list-name>":
+    "Searches a list by the specified member.",
+    "!diary <username>":
+    "Displays the recent diary entries from the specified member.",
+    "!del":
+    "Deletes the last message the bot sent in a channel."
+}
+SETTINGS["letterboxd"] = {
+    "api_base": "https://api.letterboxd.com/api/v0/",
+    "api_key": "",
+    "api_secret": ""
+}
+SETTINGS["log_channel"] = 0
+SETTINGS["mkdb_servers"] = []
+SETTINGS["tmdb"] = ""
--- src/config.py
+++
@@ -1,7 +0,0 @@
-""" Fetch the configuration values and keys from config.json
-"""
-
-import json
-
-with open('config.json') as config_file:
-    SETTINGS = json.load(config_file)
